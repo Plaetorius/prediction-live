2f31c8e588661187b599b8b063ad2677d2ea9055
Tag6362 <tag6362@gmail.com>
2025-07-13 04:28:20 +0200

    Push new models

--- types/websocket.ts
+++ types/websocket.ts
@@ -12,6 +12,7 @@ export interface ChallengeOption {
   odds?: number;
   created_at?: string;
   updated_at?: string;
+  isWinner?: boolean;
   metadata?: {
     created_at?: string;
     updated_at?: string;
